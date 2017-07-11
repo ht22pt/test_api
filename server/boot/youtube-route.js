bfa88e819281f8b2624281d392a2811baf964046
Thanh Nguyen <thanhnp@atvn.com.vn>
2017-07-11 16:07:33 +0700

    Add youtube login with oauth2 and get user channel

--- server/boot/youtube-route.js
+++ server/boot/youtube-route.js
@@ -2,32 +2,89 @@
 
 'use strict';
 
+let url = require('url');
 var YouTubeAPI = require('../utility/youtube-api');
-var logger = require('./utility/logger');
+var logger = require('../utility/logger');
 
 module.exports = function (app) {
-  // Install a `/` route that returns server status
-  var router = app.loopback.Router();
-  // var TestUserModel = app.models().TestUser;
+    // Install a `/` route that returns server status
+    var router = app.loopback.Router();
+    var TestUserModel = app.models.TestUser;
 
-  router.post('/youtube/search', function (req, res) {
-    var youtube = new YouTubeAPI();
-    youtube.searchVideo(undefined, 'demo').then(function (data) {
-      logger.debug('Debug data done', data);
-      res.send(data);
-    }).catch(function (err) {
-      res.send(err);
+    router.get('/youtube/login', function (req, res) {
+        // get authorization url
+        let baseUrl = req.headers.host;
+        if (baseUrl.indexOf('http') === -1) {
+            baseUrl = 'http://' + baseUrl;
+        }
+        let googleApi = new YouTubeAPI();
+
+        // shows dialog
+        res.redirect(googleApi.requestOAuth(baseUrl));
     });
-  });
+    router.get('/youtube/auth', function (req, res) {
+        // after user click, auth `code` will be set
+        // we'll send that and get the access token
+        let baseUrl = req.headers.host;
+        if (baseUrl.indexOf('http') === -1) {
+            baseUrl = 'http://' + baseUrl;
+        }
+        let authenCode = req.query.code;
+        let youtubeApi = new YouTubeAPI();
 
-  router.post('/youtube/comments', function (req, res) {
-    var youtube = new YouTubeAPI();
-    youtube.getComments('36m1o-tM05g').then(function (data) {
-      res.send(data);
-    }).catch(function (err) {
-      res.send(err);
+        youtubeApi.verifyOAth(baseUrl, authenCode).then(function (data) {
+            res.redirect(url.format({
+                pathname: '/youtube/channel',
+                query: data
+            }));
+        }).catch(function (err) {
+            // Log error
+            console.log(err);
+        });
     });
-  });
 
-  app.use(router);
+    router.get('/youtube/channel', function (req, res) {
+        // Get user info, store token to database and update geneate information for user
+        let token = req.query;
+        let youtubeApi = new YouTubeAPI();
+
+        youtubeApi.getChannel(token).then(function (data) {
+            // Return data to client
+            res.send(data);
+
+            let testUser = {
+                name: data.channelName,
+                type: 0,
+                token: token
+            };
+
+            return TestUserModel.upsertWithWhere({name: data.channelName}, testUser);
+        }).then(function (userInfo) {
+            logger.debug('Create user done', userInfo);
+        }).catch(function (err) {
+            // Log error
+            logger.error(err);
+        });
+    });
+
+    router.post('/youtube/search', function (req, res) {
+        var youtube = new YouTubeAPI();
+        youtube.searchVideo(undefined, 'demo').then(function (data) {
+            logger.debug('Debug data done', data);
+            res.send(data);
+        }).catch(function (err) {
+            res.send(err);
+        });
+    });
+
+    router.post('/youtube/comments', function (req, res) {
+        var youtube = new YouTubeAPI();
+        youtube.getComments('36m1o-tM05g').then(function (data) {
+            res.send(data);
+        }).catch(function (err) {
+            res.send(err);
+        });
+    });
+
+    app.use(router);
 };
--- server/utility/youtube-api.js
+++ server/utility/youtube-api.js
@@ -1,59 +1,172 @@
 /* eslint-disable promise/param-names */
 /* eslint-disable strict */
 
-var google = require('googleapis');
-// var googleAuth = require('google-auth-library');
-var logger = require('./logger');
-var API_KEY = 'AIzaSyAPck9T0HWtOh2eaVT7s2mtVAuw_rpY8a0';
+let fs = require('fs');
+let readline = require('readline');
+let google = require('googleapis');
+let googleAuth = require('google-auth-library');
+let logger = require('./logger');
+
+let API_KEY = 'AIzaSyAPck9T0HWtOh2eaVT7s2mtVAuw_rpY8a0';
+let OAUTH2_SCOPES = ['https://www.googleapis.com/auth/youtube'];
+let CLIENT_ID = '871697944833-nc91lsthdjii1io8ctqviavimftro1kt.apps.googleusercontent.com';
+let CLIENT_SECRET = 'A-A4ZsIzmxJkxPmRl9dzm5EW';
+let CLIENT_REDIRECT = '/youtube/auth';
+
+let PATH_TO_PRIVATE = '../private/';
 
 // Some API need authentication
 
-function YoutubeApi () {
-
+function YoutubeApi() {
+    this.token = '';
+    this.user = '';
+    // Start read client secret from file
+    // this.credential = JSON.parse(fs.readFileSync(PATH_TO_PRIVATE + 'google_client_secret.json'));
 }
 
-YoutubeApi.prototype.searchVideo = function (auth, searchText, nextToken) {
-  return new Promise(function (fulfill, reject) {
-    var service = google.youtube('v3');
-    service.search.list({
-      key:        API_KEY,
-      maxResults: 25,
-      part:       'snippet',
-      q:          searchText,
-      type:       ''
-    }, function (err, response) {
-      if (err) {
-        logger.error(err);
-        console.log('The API searchVideo returned an error: ' + err);
-        reject(err);
-        return;
-      }
-      fulfill(response);
+YoutubeApi.prototype.requestOAuth = function (baseUrl) {
+    let OAuth2 = google.auth.OAuth2;
+
+    let oauth2Client = new OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        baseUrl + CLIENT_REDIRECT
+    );
+
+    let url = oauth2Client.generateAuthUrl({
+        // 'online' (default) or 'offline' (gets refresh_token)
+        access_type: 'offline',
+
+        // If you only need one scope you can pass it as a string
+        scope: OAUTH2_SCOPES
+
+        // Optional property that passes state parameters to redirect URI
+        // state: { foo: 'bar' }
     });
-  });
+
+    return url;
+};
+
+YoutubeApi.prototype.verifyOAth = function (baseUrl, authenCode) {
+    let OAuth2 = google.auth.OAuth2;
+
+    let oauth2Client = new OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        baseUrl + CLIENT_REDIRECT
+    );
+
+    return new Promise(function (fulfill, reject) {
+        oauth2Client.getToken(authenCode, function (err, tokens) {
+            // Store to database
+            logger.debug(tokens);
+            if (err) {
+                logger.error(err);
+                reject(err);
+                return;
+            }
+            fulfill(tokens);
+        });
+    });
+};
+
+YoutubeApi.prototype.getChannel = function (authenCode) {
+
+    let OAuth2 = google.auth.OAuth2;
+    let oauth2Client = new OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET
+    );
+    oauth2Client.setCredentials(authenCode);
+
+    return new Promise(function (fulfill, reject) {
+
+        let service = google.youtube('v3');
+        service.channels.list({
+            auth: oauth2Client,
+            mine: 'true',
+            part: 'snippet,contentDetails,statistics'
+        }, function (err, response) {
+            if (err) {
+                logger.error(['The API channels.list error ', err]);
+                reject(err);
+                return;
+            }
+            if (Array.isArray(response.items) && response.items.length > 0) {
+                fulfill(convertChannel(response.items[0]));
+            } else {
+                fulfill({});
+            }
+        });
+    });
+};
+
+YoutubeApi.prototype.searchVideo = function (auth, searchText, nextToken) {
+    return new Promise(function (fulfill, reject) {
+        let service = google.youtube('v3');
+        service.search.list({
+            key: API_KEY,
+            maxResults: 25,
+            part: 'snippet',
+            q: searchText,
+            type: ''
+        }, function (err, response) {
+            if (err) {
+                logger.error(err);
+                console.log('The API searchVideo returned an error: ' + err);
+                reject(err);
+                return;
+            }
+            fulfill(response);
+        });
+    });
 };
 
 YoutubeApi.prototype.getComments = function (videoId) {
-  return new Promise(function (fulfill, reject) {
-    videoId = '36m1o-tM05g';
-    var service = google.youtube('v3');
-    service.commentThreads.list({
-      key:     API_KEY,
-      part:    'snippet,replies',
-      videoId: videoId
-    }, function (err, response) {
-      if (err) {
-        console.log('The API getComments returned an error: ' + err);
-        reject(err);
-        return;
-      }
-      fulfill(response);
+    return new Promise(function (fulfill, reject) {
+        videoId = '36m1o-tM05g';
+        let service = google.youtube('v3');
+        service.commentThreads.list({
+            key: API_KEY,
+            part: 'snippet,replies',
+            videoId: videoId
+        }, function (err, response) {
+            if (err) {
+                console.log('The API getComments returned an error: ' + err);
+                reject(err);
+                return;
+            }
+            fulfill(response);
+        });
     });
-  });
 };
 
 // ============================================ //
 // ============= CONVERT METHOD =============== //
 // ============================================ //
 
+function convertChannel (channelData) {
+    var convertData = {};
+    convertData.id           = channelData.id;
+    convertData.channelName  = channelData.snippet.title;
+    convertData.description  = channelData.snippet.description;
+    convertData.publishedAt  = channelData.snippet.publishedAt;
+    convertData.thumbnails   = channelData.snippet.thumbnails;
+    convertData.commentCount = channelData.statistics.commentCount;
+    convertData.videoCount   = channelData.statistics.videoCount;
+    convertData.viewCount    = channelData.statistics.viewCount;
+    return convertData;
+}
+
+function convertVideos (rawVideoList) {
+    var convertData = {};
+    return convertData;
+}
+
+function convertComments (rawCommentList) {
+    var convertData = {};
+    return convertData;
+}
+
+// Exports
 module.exports = YoutubeApi;
